49ca06ae204a6353f8f945114e31de61e121350a
joliveros <oliverosjose@gmail.com>
2016-11-21 17:58:42 -0430

    trabajando en hibernate

--- Ejemplo1_jar/src/main/java/com/joseoliveros/pruebas/Prueba.java
+++ Ejemplo1_jar/src/main/java/com/joseoliveros/pruebas/Prueba.java
@@ -1,9 +1,34 @@
 package com.joseoliveros.pruebas;
 
+import com.joseoliveros.Libro;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Prueba {
 
-    public static void main(String[] args) throws Exception {
-        Test test = new Test();
-        test.hacerAlgo();
+    private static final Logger log = Logger.getLogger(Prueba.class.getPackage().getName());
+
+    public static void main(String[] args) {
+
+        Session session = null;
+        Transaction transaction = null;
+        SessionFactory factory = null;
+
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+            session = factory.openSession();
+            Libro libro = (Libro) session.get(Libro.class, "8");
+            session.delete(libro);
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            transaction.rollback();
+        } finally {
+            session.close();
+            factory.close();
+        }
     }
 }
