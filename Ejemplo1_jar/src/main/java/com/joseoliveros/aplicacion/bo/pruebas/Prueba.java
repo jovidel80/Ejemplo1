c6420df905d869171bcb869eea0befe707382214
jose <oliverosjose@gmail.com>
2016-11-26 19:43:18 -0430

    listo Hibernate Relaciones

--- Ejemplo1_jar/src/main/java/com/joseoliveros/aplicacion/bo/pruebas/Prueba.java
+++ Ejemplo1_jar/src/main/java/com/joseoliveros/aplicacion/bo/pruebas/Prueba.java
@@ -1,12 +1,11 @@
 package com.joseoliveros.aplicacion.bo.pruebas;
 
+import com.joseoliveros.aplicacion.bo.HibernateHelper;
 import com.joseoliveros.aplicacion.bo.Libro;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class Prueba {
@@ -15,12 +14,19 @@ public class Prueba {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("arquitecturaJava");
-        EntityManager em = emf.createEntityManager();
-        TypedQuery<Libro> consulta = em.createQuery("select l from Libro l", Libro.class);
-        List<Libro> lista = consulta.getResultList();
-        for (Libro libro : lista) {
-            System.out.println(libro);
+        SessionFactory sessionFactory = HibernateHelper.getSessionFactory();
+        Session session = sessionFactory.openSession();
+//        Categoria categoria = new Categoria();
+//        categoria.setId("8");
+//        categoria.setDescripcion("prueba descripcion");
+//        Libro libro = new Libro("9", "prueba", categoria);
+//        libro.insertar();
+        List<Libro> listaDeLibros = session.createQuery("from Libro libro").list();
+        for (Libro libro : listaDeLibros) {
+            System.out.println(libro.getIsbn());
+            System.out.println(libro.getTitulo());
+            System.out.println(libro.getCategoria().getDescripcion());
         }
+        session.close();
     }
 }
